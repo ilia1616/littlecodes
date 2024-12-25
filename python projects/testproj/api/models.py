3e7e964d52767c1a3501e5ea11e3d9c2d89b6ae2
seyed ali <miriali946@gmail.com>
2024-12-25 08:29:44 +0330

    added api/create

--- python projects/testproj/api/models.py
+++ python projects/testproj/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=10) # it is a STRING and max char is 10
-    password = models.CharField(max_length=12) # you HAVE to define max len
+    username = models.CharField(max_length=100) # it is a STRING and max char is 10
+    password = models.CharField(max_length=102) # you HAVE to define max len
     
     def __str__(self):
         return self.name
--- python projects/testproj/api/views.py
+++ python projects/testproj/api/views.py
@@ -2,8 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
-from .jsonizer import UseSerializer
+from .jsonizer import UserJsonizer
 
 @api_view(['GET'])
 def get_user(request):
-    return Response(UseSerializer({'username':'yaghob','password':'yaghob'}))
+    return Response(UserJsonizer({'username':'admin',
+                                  'password':'admin'}).data)
+    
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserJsonizer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, 
+                    status=status.HTTP_400_BAD_REQUEST)
