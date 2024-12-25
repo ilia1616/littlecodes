19628de578fc1ff2e3ca0580f704cf33fbd30704
seyed ali <miriali946@gmail.com>
2024-12-25 19:00:21 +0330

    added the database information and pymysql to setttings.py

--- python projects/testproj/testproj/settings.py
+++ python projects/testproj/testproj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'testproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER' : 'ilia',
+        'PASSWORD':'ilia',
+        'HOST':'192.168.100.128',
+        'PORT':'3306'
     }
 }
 
