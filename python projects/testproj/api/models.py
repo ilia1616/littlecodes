2522f39e20726a85f8b7488176270b3ba417dc62
seyed ali <miriali946@gmail.com>
2024-12-25 07:48:57 +0330

    created a db with(python manage.py makemigrations & python manage.py migrate)

--- python projects/testproj/api/models.py
+++ python projects/testproj/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=10) # it is a STRING and max char is 10
-    password = models.CharField()
+    password = models.CharField(max_length=12) # you HAVE to define max len
     
     def __str__(self):
         return self.name
