1e51854094fc87b7c79ea3e2fed22249ca149c30
seyed ali <miriali946@gmail.com>
2024-12-25 07:47:05 +0330

    define and creating model(our data) in api>models.py

--- python projects/testproj/api/models.py
+++ python projects/testproj/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=10) # it is a STRING and max char is 10
+    password = models.CharField()
+    
+    def __str__(self):
+        return self.name
+    
