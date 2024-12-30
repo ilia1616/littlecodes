f6f72b4b598b2b02bf67fecdfd445e23b8cfbb8f
seyed ali <miriali946@gmail.com>
2024-12-30 12:32:15 +0330

    i failed will try again with a youtube video

--- React/frontend/src/components/category-box.js
+++ React/frontend/src/components/category-box.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CategoryBox = ({category, items}) => {
+    const nav = useNavigate();
+
     return(
         <div className="category-box">
             <h2>{category}</h2>
             <ul>
                 {items.map((item,index) => 
-                <li key={index}>{item}</li>
+                <li key={index}>
+                    <button onClick={() => nav("http://localhost:3000/" + {item})}>
+
+                    </button>
+                    </li>
                 )}
             </ul>
         </div>
