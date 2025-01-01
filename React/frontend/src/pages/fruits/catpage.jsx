17b1acfd25ec38fb98ad8a0efc48f25eb5a36658
seyed ali <miriali946@gmail.com>
2025-01-01 19:45:23 +0330

    added an "if" to catpage.jsx

--- React/frontend/src/pages/fruits/catpage.jsx
+++ React/frontend/src/pages/fruits/catpage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import CatPage from '../../components/category-page'
+import { categories } from '../../data'
 
-export default function Catpage() {
+export default function the_page(specific_category) {
   return (
-    <div className='App'>
-        {cate}
+   <div className='App'>
+      {categories.map((cat) =>
+        cat.name === specific_category ? (
+          <CatPage key={cat.id} name={cat.name} />
+        ) : null
+      )}
     </div>
   )
 }
