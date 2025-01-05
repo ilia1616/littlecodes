3cc5652c98126fc94d1734288bba8f86f49c6942
seyed ali <miriali946@gmail.com>
2025-01-05 10:36:54 +0330

    create_item works fine

--- python projects/testproj/api/urls.py
+++ python projects/testproj/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_user, create_item, auth, get_item
+from .views import get_item, create_item
 
-urlpatterns =[path('user/<int:pk>', get_user ,name='get_user'),
-              path('create/', create_item,name='create_user'),
-              path('auth/', auth, name="authentication"),
-              path('getitem/<int:pk>', get_item, name='get_item')
+urlpatterns =[
+              path('getitem/<int:pk>', get_item, name='get_item'),
+            #   path('user/<int:pk>', get_user ,name='get_user'),
+              path('create/', create_item, name='create_user'),
+            #   path('auth/', auth, name="authentication"),
               ]
 # this file most have urlpatterns
 # if not the server wont run
--- python projects/testproj/api/views.py
+++ python projects/testproj/api/views.py
@@ -15,11 +15,11 @@ def get_item(request,pk):
     return Response(ser.data)
     
 
-@api_view(['GET'])
-def get_user(request, pk):
-    usr = User.objects.get(pk=pk)
-    serializer = UserJsonizer(usr)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def get_user(request, pk):
+#     usr = User.objects.get(pk=pk)
+#     serializer = UserJsonizer(usr)
+#     return Response(serializer.data)
     
 @api_view(['POST'])
 def create_item(request):
@@ -37,18 +37,18 @@ def create_item(request):
 
 
 
-@api_view(['POST'])
-def auth(request):
-    serializer = UserJsonizer(data=request.data)
-    if serializer.is_valid():
-        username = serializer.validated_data.get('username')
-        password = serializer.validated_data.get("password")
-        try:
-            tmpuser = User.objects.get(username=username)
-            if username == tmpuser.username and password == tmpuser.password:
-               return Response("Access granted", status=status.HTTP_202_ACCEPTED)
-            else:
-               return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
-        except:
-            return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# def auth(request):
+#     serializer = UserJsonizer(data=request.data)
+#     if serializer.is_valid():
+#         username = serializer.validated_data.get('username')
+#         password = serializer.validated_data.get("password")
+#         try:
+#             tmpuser = User.objects.get(username=username)
+#             if username == tmpuser.username and password == tmpuser.password:
+#                return Response("Access granted", status=status.HTTP_202_ACCEPTED)
+#             else:
+#                return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+#         except:
+#             return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
